94106093524789acaca2fca10196afca0ca24c31
lzj <leezj9671@gmail.com>
2017-08-04 23:41:42 +0800

    view add in main

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 from sylfk import SYLFk
+from sylfk.view import Controller
+from core.base_view import BaseView
 
+class Index(BaseView):
+	def get(self, request):
+		return 'get request HALO'
+
+class Test(Index):
+	def post(self, request):
+		return 'post requet'
 
 app = SYLFk()
+url_map = [
+	{
+		'url': '/shiyanlou',
+		'view': Index,
+		'endpoint': 'index'
+	},
+	{
+		'url': '/test',
+		'view': Test,
+		'endpoint': 'test'
+	}
 
-@app.route('/index', methods=['GET'])
-def index():
-    return '这是一个路由测试页面'
+]
+
+index_controller = Controller('index', url_map)
+app.load_controller(index_controller)
+# @app.route('/index', methods=['GET'])
+# def index():
+#     return '这是一个路由测试页面'
 
 
-@app.route('/test/js')
-def test_js():
-   return '<script src="/static/test.js"></script>'
+# @app.route('/test/js')
+# def test_js():
+#    return '<script src="/static/test.js"></script>'
 
 app.run()
